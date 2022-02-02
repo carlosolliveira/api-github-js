d2009482a29fbf543bbc63627516cd5b250425d9
Carlos <vxcarlosig@gmail.com>
2022-02-02 13:24:52 -0300

    Bug da requisição dos repositorios ok, agora so falta extrair os dados e inseri-los na pagina

--- js/preview.js
+++ js/preview.js
@@ -1,4 +1,11 @@
+const prevrepo = (repos) => {
+    for(r in repos){
+        let nome = r.name
+    }
+}
+
 const prev = (data) => {
+    console.log(data)
     let nome = data.name
     let username = data.login
     let avatar = data.avatar_url
@@ -14,6 +21,26 @@ const prev = (data) => {
     document.querySelector("#qountrepo").innerText = repopublic + " Repositorios"
     let div = document.createElement('div')
     let aside = document.createElement('aside')
+    const getrepo = async (url) => {
+        var reponse = await fetch(url)
+        if(reponse.status == 200){
+            return await reponse.json()
+        }
+    }
+    getrepo(data.repos_url).then((data) => {
+        if(typeof data != 'undefined'){
+            if(data.length != 0){
+                prevrepo(data)
+            }
+            else{
+                let repo0 = document.querySelector("#repo0")
+                repo0.style.display = 'block'
+            }
+        }
+        else{
+            alert("Erro")
+        }
+    })
 }
 
 export {prev};
